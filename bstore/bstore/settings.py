d7bf91527495c1a6608cd3a805f96ed047b0ac83
Casey24 <caseykinsley77@gmail.com>
2023-12-13 16:35:18 -0500

    config

--- bstore/bstore/settings.py
+++ bstore/bstore/settings.py
@@ -55,7 +55,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = 'bstore.urls'
 
