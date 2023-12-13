29b7c052a98a7324463f83c8a04e4f464ccff7bf
Casey24 <caseykinsley77@gmail.com>
2023-12-13 15:45:19 -0500

    vercel settings

--- bstore/bstore/settings.py
+++ bstore/bstore/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles_build', "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/images/'
 
